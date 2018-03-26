314767773bdbc07649cf98c92d999d23d180f80d
OlesyaTroyanovska <37290865+OlesyaTroyanovska@users.noreply.github.com>
2018-03-26 10:56:52 +0300

    Add files via upload

---
+++ Labor1/ArraySort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+
+public class ArraySort {
+
+    public static void main(String[] args) {
+        int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+
+        //PUT YOUR CODE HERE
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i] > array[j]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+
+                }
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+    }}}
+
+
+
+
+
+
+
